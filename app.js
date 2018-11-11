6e06c27c71b2ae82fafff992973dd26b9c3d07f1
= <=>
2018-11-11 08:09:02 -0600

    auth stuff

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/auth', indexRouter);
-app.use("/login", loginRouter);
+app.use("/app/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
