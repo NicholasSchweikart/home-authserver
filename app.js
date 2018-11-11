870b387e0a7a16ce511ef58ee02e05980aef0708
= <=>
2018-11-11 08:11:17 -0600

    removed CORS

--- app.js
+++ app.js
@@ -43,11 +43,11 @@ app.use(session(
   },
 ));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use('/auth', indexRouter);
 app.use("/app/login", loginRouter);
